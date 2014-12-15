754a58a6a7016ea5587704c7d2acbe300a049376
Alexandros Ntavelos <a.ntavelos@gmail.com>
2014-12-15 14:04:35 +0100

    Initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+# GLOBAL SETTINGS
+# 
+from colorama import init as colorama_init, Fore, Back, Style
+
+CHANNELS = (
+	('cnn', "http://rss.cnn.com/rss/edition_world.rss"),
+	('bbc', "http://feeds.bbci.co.uk/news/rss.xml"),
+	('reuters', "http://feeds.reuters.com/news/artsculture"),
+)
+
+OUTPUT_DECORATORS = 
