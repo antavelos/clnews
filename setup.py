8b82cd757e9aca36b948728b0a78eebe66f6b9d4
Alexandros Ntavelos <a.ntavelos@gmail.com>
2014-12-21 01:47:12 +0100

    PyPi configuration

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='CLNews',
+    version='0.1.0',
+    author='Alexandros Ntavelos',
+    author_email='a.ntavelos@gmail.com',
+    packages=['clnews', 'clnews.test'],
+    # scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/CLNews/',
+    license='LICENSE.txt',
+    description='Advanced news feed reader',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "colorama==0.3.2",
+        "feedparser==5.1.3",
+        "termcolor==1.1.0",
+    ],
+)
